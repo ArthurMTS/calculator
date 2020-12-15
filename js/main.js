43092df42e41be6e42cf30bc48ad45cfe29a5ccd
Arthur Silva <arthurmatsilva@outlook.com>
2020-12-15 16:11:24 -0300

    Initial GUI

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-alert('Hello, world!');
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
+
+console.log(operate('+', 7, 10));
+console.log(operate('-', 5, 2));
+console.log(operate('-', 2, 9));
+console.log(operate('/', 25, 5));
+console.log(operate('*', 3, 5));

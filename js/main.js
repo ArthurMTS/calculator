907a64be8a291d33adf02601c1b7e4e60272efda
Arthur Silva <arthurmatsilva@outlook.com>
2023-08-28 19:32:58 -0300

    refactor: updating and cleaning the code

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
-const view = document.querySelector('#view');
+const view = document.querySelector("#view");
 let firstValue = 0;
 let secoundValue = 0;
-let operator = '';
+let operator = "";
 let clearView = false;
 
-const add = (num1, num2) => num1 + num2;
+const add = (x, y) => x + y;
 
-const subtract = (num1, num2) => num1 - num2;
+const subtract = (x, y) => x - y;
 
-const multiply = (num1, num2) => num1 * num2;
+const multiply = (x, y) => x * y;
 
-const divide = (num1, num2) => num1 / num2;
+const divide = (x, y) => {
+  if (y === 0) {
+    acFunc();
+    view.value = "Don't think so!";
+    return;
+  }
+  return x / y;
+};
 
-function operate(operator, num1, num2) {
-  num1 = Number(num1);
-  num2 = Number(num2);
+function operate(operator, x, y) {
+  x = Number(x);
+  y = Number(y);
 
   switch (operator) {
-    case '+':
-      return add(num1, num2);
-    case '-':
-      return subtract(num1, num2);
-    case 'x':
-      return multiply(num1, num2);
-    case '/':
-      return divide(num1, num2);
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "x":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
   }
 }
 
@@ -36,9 +43,8 @@ function display(value) {
 }
 
 function digitFunc(digit) {
-
   if (view.value.length > 9) {
-    alert('To many digits');
+    alert("To many digits");
     return;
   }
 
@@ -55,24 +61,17 @@ function acFunc() {
 
   firstValue = 0;
   secoundValue = 0;
-  operator = '';
+  operator = "";
   clearView = false;
 }
 
 function backspaceFunc() {
-
   if (view.value == 0) return;
-  else if (view.value > 0 && view.value < 10) view.value = 0;
-  else {
-    const string = view.value.split('');
-    string.pop();
-
-    display(string.join(''));
-  }
+  else if (view.value < 10) view.value = 0;
+  else display(view.value.slice(0, -1));
 }
 
 function operatorFunc(op) {
-  
   if (firstValue != 0) equalFunc();
 
   firstValue = view.value;
@@ -84,39 +83,24 @@ function equalFunc() {
 
   secoundValue = view.value;
 
-  if (operator == '/' && secoundValue == 0) {
-    acFunc();
-    view.value = 'Not today!';
-    return;
-  }
-
   const result = operate(operator, firstValue, secoundValue);
   acFunc();
   display(result);
 }
 
-const digits = document.querySelectorAll('.digits');
-digits.forEach(digit => digit.addEventListener('click', ({ path }) => digitFunc(path[0].textContent)));
+const digits = document.querySelectorAll(".digit");
+digits.forEach((digit) =>
+  digit.addEventListener("click", ({ target }) => digitFunc(target.textContent))
+);
 
-const ac = document.querySelector('#ac');
-ac.addEventListener('click', acFunc);
+const ac = document.querySelector("#ac");
+ac.addEventListener("click", acFunc);
 
-const backspace = document.querySelector('#backspace');
-backspace.addEventListener('click', backspaceFunc);
+const backspace = document.querySelector("#backspace");
+backspace.addEventListener("click", backspaceFunc);
 
-const operators = document.querySelectorAll('.operator');
-operators.forEach(op => op.addEventListener('click', () => operatorFunc(op)));
+const operators = document.querySelectorAll(".operator");
+operators.forEach((op) => op.addEventListener("click", () => operatorFunc(op)));
 
-const equal = document.querySelector('#equal');
-equal.addEventListener('click', equalFunc);
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', () => {
-  button.style.transform = 'scale(0.95)';
-}));
-
-buttons.forEach(button => button.addEventListener('transitionend', (e) => {
-  if (e.propertyName !== 'transform') return;
-  
-  e.target.style.transform = 'scale(1)';
-}));
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", equalFunc);
